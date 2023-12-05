a1c5ca5d1b9e915806840fd1cc12a8cf187a3036
Vlad Kazanbaev <vladkazanbaev56@gmail.com>
2023-12-05 17:23:45 +0500

    pr_2

--- Pr_1/Form1.Designer.cs
+++ Pr_1/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtd = new System.Windows.Forms.TextBox();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 1);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(623, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // txtd
+            // 
+            this.txtd.Location = new System.Drawing.Point(330, 329);
+            this.txtd.Name = "txtd";
+            this.txtd.Size = new System.Drawing.Size(100, 20);
+            this.txtd.TabIndex = 1;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(12, 329);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.TabIndex = 2;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(118, 329);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 3;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(224, 329);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(607, 373);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.txtd);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtd;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Pr_1/Form1.cs
+++ Pr_1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Pr_1
 {
@@ -15,6 +16,30 @@ namespace Pr_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+            double c = double.Parse(txtc.Text);
+            double step = double.Parse(txtd.Text);
+
+            // Очищаем график перед построением нового
+            chart1.Series.Clear();
+            // Создаем новый ряд данных для графика
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+
+            // Вычисляем значения функции и добавляем точки к ряду данных
+            for (double x = -10; x <= 10; x += step)
+            {
+                double y = a * x * x + b * x + c;
+                series.Points.AddXY(x, y);
+            }
+
+            // Добавляем ряд данных на график
+            chart1.Series.Add(series);
         }
     }
 }
